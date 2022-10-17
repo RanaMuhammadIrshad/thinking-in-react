b3a2a5c794d286b4d9a5cbd494e587792c49fd9d
RanaMuhammadIrshad <rmirshad@web.de>
2022-10-17 15:47:37 +0200

    Updates

---
+++ precache-manifest.4f4937ff23d68c7ffd1546be8f17c76a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94e621722ede28c9fcb661724ccfcb1",
+    "url": "/thinking-in-react/index.html"
+  },
+  {
+    "revision": "d48717ac56bc0f185276",
+    "url": "/thinking-in-react/static/js/2.9640c0e7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/thinking-in-react/static/js/2.9640c0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf7589c8608e0f0f9cf",
+    "url": "/thinking-in-react/static/js/main.48eb8be5.chunk.js"
+  },
+  {
+    "revision": "bfb97644df0cf4bea3f6",
+    "url": "/thinking-in-react/static/js/runtime-main.6ab0459f.js"
+  }
+]);
